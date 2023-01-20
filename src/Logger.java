1285d0817235256b59281e17fda89603e32f6e38
PowerOleg <geraskin.oleg@mail.ru>
2023-01-20 17:39:06 +0500

    Logger is in progress. 2.1-snapshot

--- src/Logger.java
+++ src/Logger.java
@@ -1,2 +1,16 @@
 public class Logger {
+    protected int num = 1;
+    public static Logger logger;
+
+    private Logger() {
+    }
+
+    public void log(String msg) {
+        System.out.println("[" + num++ + "] " + msg);
+    }
+    public static Logger getInstance() {
+        if (logger == null) logger = new Logger();
+        return logger;
+    }
+
 }
